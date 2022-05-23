62b71b6801c455b442e8551684da9e5a399db541
peruibeloko <cmendonca32@gmail.com>
2022-05-22 22:17:59 -0300

    fix build and spa config for gh pages

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,17 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  base: './',
-  build: {
-    outDir: 'docs'
-  }
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    plugins: [vue()],
+    build: {
+      outDir: 'docs'
+    },
+    define: {
+      VITE_BACKEND_URL: env.VITE_BACKEND_URL
+    }
+  };
 });
